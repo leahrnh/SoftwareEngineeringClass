575cd1097008878dfd0dfa28233534767011c793
Leah <eahlay@gmail.com>
2015-09-21 09:10:14 -0400

    wrote TokenAnnotator

--- maven.1441823367401/pi3-lnicolic/src/main/java/Consumer.java
+++ maven.1441823367401/pi3-lnicolic/src/main/java/Consumer.java
@@ -15,7 +15,7 @@ import org.apache.uima.resource.ResourceProcessException;
 public class Consumer extends CasConsumer_ImplBase {
 	
 	private int mDocNum;
-	private File mOutputDir = new File("outputData");
+	private File mOutputDir = new File("src/main/resources/outputData");
 	
 	public void initialize() throws ResourceInitializationException {
 		mDocNum = 0;
@@ -32,38 +32,6 @@ public class Consumer extends CasConsumer_ImplBase {
 		  } catch (CASException e) {
 		    throw new ResourceProcessException(e);
 		  }
-		 
-		  // retrieve the filename of the input file from the CAS
-		  /*FSIterator it = jcas.getAnnotationIndex(SourceDocumentInformation.type).iterator();
-		  File outFile = null;
-		  if (it.hasNext()) {
-		    SourceDocumentInformation fileLoc = (SourceDocumentInformation) it.next();
-		    File inFile;
-		    try {
-		      inFile = new File(new URL(fileLoc.getUri()).getPath());
-		      String outFileName = inFile.getName();
-		      if (fileLoc.getOffsetInSource() > 0) {
-		        outFileName += ("_" + fileLoc.getOffsetInSource());
-		      }
-		      outFileName += ".xmi";
-		      outFile = new File(mOutputDir, outFileName);
-		      modelFileName = mOutputDir.getAbsolutePath() + 
-		            "/" + inFile.getName() + ".ecore";
-		    } catch (MalformedURLException e1) {
-		      // invalid URL, use default processing below
-		    }
-		  }
-		  if (outFile == null) {
-		    outFile = new File(mOutputDir, "doc" + mDocNum++);
-		  }
-		  // serialize XCAS and write to output file
-		  try {
-		    writeXmi(jcas.getCas(), outFile, modelFileName);
-		  } catch (IOException e) {
-		    throw new ResourceProcessException(e);
-		  } catch (SAXException e) {
-		    throw new ResourceProcessException(e);
-		  }*/
 
 	}
 
